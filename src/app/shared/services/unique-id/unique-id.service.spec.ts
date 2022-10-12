1bb65eaa7ed74be531cd67af4edb108fd41c391f
Anderson Silva <andssilva.23@gmail.com>
2022-10-12 15:46:36 -0400

    feat: :rocket: criando um teste para verificar se existem ids repetidos

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -6,4 +6,14 @@ describe(UniqueIdService.name, () => {
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBe(true);
   });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate id when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    };
+
+    expect(ids.size).toBe(50);
+  })
 });
