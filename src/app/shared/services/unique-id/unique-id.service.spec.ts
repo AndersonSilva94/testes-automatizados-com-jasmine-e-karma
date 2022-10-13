55d4b8a585a9b7772e49b5f302d17e2910a07344
Anderson Silva <andssilva.23@gmail.com>
2022-10-12 21:13:06 -0400

    feat: :rocket: testando exceções (throw)

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -7,12 +7,14 @@ describe(UniqueIdService.name, () => {
     service = new UniqueIdService();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBe(true);
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate id when called multiple times`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicate id when called multiple times`, () => {
     const ids = new Set();
     for (let i = 0; i < 50; i++) {
       ids.add(service.generateUniqueIdWithPrefix('app'));
@@ -21,9 +23,18 @@ describe(UniqueIdService.name, () => {
     expect(ids.size).toBe(50);
   })
 
-  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generateIds when called`, () => {
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generateIds when called`, () => {
     service.generateUniqueIdWithPrefix('app');
     service.generateUniqueIdWithPrefix('app');
     expect(service.getNumberOfGeneratedUniqueIds()).toBe(2)
   });
+
+  it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should throw when called with empty`, () => {
+      const emptyValues = [null, undefined, ''];
+      emptyValues.forEach(emptyValue => {
+        expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow() // ao lançar o erro, precisamos passar o expect com uma arrow function
+      })
+    })
 });
